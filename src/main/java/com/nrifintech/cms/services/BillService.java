4369005b6b8339fc16d4286a0deb0b9de249d00f
Gautam Chandra Saha <devgautam1231@gmail.com>
2023-03-05 16:26:33 +0530

    added calender months

--- src/main/java/com/nrifintech/cms/services/BillService.java
+++ src/main/java/com/nrifintech/cms/services/BillService.java
@@ -10,4 +10,6 @@ public class BillService {
 
 	@Autowired
 	private BillRepo billRepo;
+	
+//	TODO: complete the service
 }
--- src/main/java/com/nrifintech/cms/utils/Calender.java
+++ src/main/java/com/nrifintech/cms/utils/Calender.java
@@ -3,26 +3,37 @@ package com.nrifintech.cms.utils;
 import java.util.HashMap;
 import java.util.Map;
 
-enum WeekDay{
-	Sunday,
-	Monday,
-	Tuesday,
-	Wednesday,
-	Thursday,
-	Friday,
-	Saturday
+enum WeekDay {
+	Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
 }
 
 public class Calender {
 
-	public static Map<WeekDay,Integer> days =  new HashMap<>();
-	
+	public static Map<WeekDay, Integer> days = new HashMap<>();
+
 	static {
-		days.put( WeekDay.Monday,1);
-		days.put( WeekDay.Tuesday,2);
-		days.put( WeekDay.Wednesday,3);
-		days.put( WeekDay.Thursday,4);
-		days.put( WeekDay.Friday,5);
+		days.put(WeekDay.Monday, 1);
+		days.put(WeekDay.Tuesday, 2);
+		days.put(WeekDay.Wednesday, 3);
+		days.put(WeekDay.Thursday, 4);
+		days.put(WeekDay.Friday, 5);
 	}
-	
+
+	public enum Month {
+		NONE, January, February, March, April, May, June, July, August, September, November, December
+
+	};
+
+	public static Integer getDays(Month month, Integer year) {
+
+		int arr[] = { -1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		Integer m = month.ordinal();
+		// For checking leap year
+		if (month.equals(Month.February) && ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0))))
+			return (arr[m] + 1);
+
+		return arr[m];
+	}
+
 }
