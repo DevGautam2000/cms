5313fa78854b7733cb07397feaa1db2143fef8a2
Sagnik Sinha <sagniks@trainee.nrifintech.com>
2023-03-13 09:54:56 +0530

    fixed imports

--- src/main/java/com/nrifintech/cms/repositories/OrderRepo.java
+++ src/main/java/com/nrifintech/cms/repositories/OrderRepo.java
@@ -2,9 +2,11 @@ package com.nrifintech.cms.repositories;
 
 import javax.transaction.Transactional;
 
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.nrifintech.cms.entities.Order;
 
 public interface OrderRepo extends JpaRepository<Order,Integer>{
