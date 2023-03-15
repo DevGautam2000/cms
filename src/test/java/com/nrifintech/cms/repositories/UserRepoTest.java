aa9cdffee7629b55c3a57decfd72f4d4b4370fd9
Mayukh Barman Ray <mayukhr@trainee.nrifintech.com>
2023-03-15 11:15:01 +0530

    added some testing classes

--- src/test/java/com/nrifintech/cms/repositories/UserRepoTest.java
+++ src/test/java/com/nrifintech/cms/repositories/UserRepoTest.java
@@ -2,11 +2,19 @@ package com.nrifintech.cms.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.security.Timestamp;
+import java.util.Date;
+
+import org.apache.tomcat.jni.Time;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.nrifintech.cms.entities.User;
+import com.nrifintech.cms.types.EmailStatus;
 import com.nrifintech.cms.types.Role;
 import com.nrifintech.cms.types.Status;
 import com.nrifintech.cms.types.UserStatus;
@@ -15,22 +23,27 @@ import com.nrifintech.cms.types.UserStatus;
 public class UserRepoTest {
     @Autowired
     private UserRepo userRepo;
-    // @Test
-    // void testFindByEmail() {
-    //     //given
-    //     String email="abcd@gmail.com";
-    //     User userIn= new User(1, null, email, 
-    //     "123",  "9876543210", Role.Admin, UserStatus.Active ,
-    //      null, null, null, null);
 
-    //     userRepo.save(userIn);
+    @BeforeEach
+    void setup(){
+        String email="abcdzx@gmail.com";
+        User userIn= new User(999, null, email, 
+        "123",  "9876543210", Role.Admin, UserStatus.Active ,EmailStatus.subscribed, null
+        , null, null,null);
+        userRepo.save(userIn);
+    }
 
-    //     //when
-    //     User userActual = userRepo.findByEmail(email).orElse(null);
+    @AfterEach
+    void destroy(){
+        userRepo.deleteById(999);
+    }
 
-    //     //then
-    //     // assert(user.);
-    // }
+    @Test
+    void testFindByEmail() {
+        String user="abcd@gmail.com";
+        User userActual = userRepo.findByEmail(user).orElse(null);
+        assert(userActual.getEmail().equals(user));
+    }
 
     @Test
     void testGetUserByOrderDate() {
