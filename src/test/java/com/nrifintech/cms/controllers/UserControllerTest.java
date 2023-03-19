5cf8583274595b26f9fd08abe1225437a48cb2fd
Mayukh Barman Ray <mayukhr@trainee.nrifintech.com>
2023-03-20 00:11:01 +0530

    partial menu service testing done

--- src/test/java/com/nrifintech/cms/controllers/UserControllerTest.java
+++ src/test/java/com/nrifintech/cms/controllers/UserControllerTest.java
@@ -1,21 +1,108 @@
 package com.nrifintech.cms.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-// @SpringBootTest
+import com.nrifintech.cms.entities.User;
+import com.nrifintech.cms.routes.Route;
+import com.nrifintech.cms.services.UserService;
+import com.nrifintech.cms.types.EmailStatus;
+import com.nrifintech.cms.types.Response;
+import com.nrifintech.cms.types.Role;
+import com.nrifintech.cms.types.UserStatus;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+    @Autowired
+	private MockMvc mockMvc;
 
+    @InjectMocks
+    private UserController userController;
 
-    
-    @Test
-    void testAddUser() {
+    @Mock(lenient = true)
+    private UserService userService;
+
+    @Mock(lenient = true)
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Mock(lenient = true)
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setup(){
+        User user = new User(999,"avatar.png","abc@gamil.com","password","9876543210",Role.User,UserStatus.Active,EmailStatus.subscribed,null,
+        null,null,null);
+
+        // when(passwordEncoder.encode(any())).thenReturn("password");
+        when(userService.addUser(user)).thenReturn(user);
 
     }
 
     @Test
-    void testGetEmailStatus() {
+    public void testAddUser() {
+        MockitoAnnotations.initMocks(this);
 
+        User user = new User(999,"avatar.png","abc@gamil.com","password","9876543210",Role.User,UserStatus.Active,EmailStatus.subscribed,null,
+        null,null,null);
+
+        when(passwordEncoder.encode(any())).thenReturn("password");
+        when(userService.addUser(user)).thenReturn(user);
+        // when(applicationEventPublisher.publishEvent(any())).thenReturn(any());
+
+        Response response = userController.addUser(user);
+
+        verify(passwordEncoder, times(1)).encode(any());
+        verify(userService, times(1)).addUser(user);
+        // verify(applicationEventPublisher, times(0)).publishEvent(any());
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Response.JsonEntity(message=User added., status=200)", response.getBody().toString());
+    }
+
+    @Test
+    void testGetEmailStatus() throws Exception {
+
+
+
+        this.mockMvc.perform(get(Route.User.prefix + Route.User.getEmailStatus))
+        .andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Hello, Mock")));
+
+        // mockMvc.perform(MockMvcRequestBuilders
+        // .get(Route.User.getEmailStatus)
+        // .accept(MediaType.APPLICATION_JSON)
+        // .andDo(print())
+        // .andExpect(status().isOk())
+        // .andExpect(MockMvcResultMatchers.jsonPath("$.employees").exists())
+        // .andExpect(MockMvcResultMatchers.jsonPath("$.employees[*].employeeId").isNotEmpty());
     }
 
     @Test
---
+++ src/test/java/com/nrifintech/cms/services/MenuServiceTest.java
@@ -0,0 +1,166 @@
+package com.nrifintech.cms.services;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nrifintech.cms.entities.Item;
+import com.nrifintech.cms.entities.Menu;
+import com.nrifintech.cms.errorhandler.NotFoundException;
+import com.nrifintech.cms.repositories.MenuRepo;
+import com.nrifintech.cms.types.Approval;
+import com.nrifintech.cms.types.ItemType;
+import com.nrifintech.cms.types.MealType;
+
+@SpringBootTest
+public class MenuServiceTest {
+    private List<Item> items= new ArrayList<>();
+    private List<Menu> menues = new ArrayList<>();
+
+	@Mock
+	private MenuRepo menuRepo;
+
+	@Mock
+	private ItemService itemService;
+
+    @InjectMocks
+    private MenuService menuService;
+
+    @BeforeEach
+    void setup(){
+        items.add(new Item(999,12,10.0,ItemType.Veg,"abc.png","tasty","food1"));
+        items.add(new Item(998,13,10.0,ItemType.Veg,"abc.png","tasty","food2"));
+        items.add(new Item(997,14,10.0,ItemType.Veg,"abc.png","tasty","food3"));
+        items.add(new Item(996,15,10.0,ItemType.NonVeg,"abc.png","yum","food4"));
+        items.add(new Item(995,16,10.0,ItemType.NonVeg,"abc.png","yum","food5"));
+        items.add(new Item(994,17,10.0,ItemType.NonVeg,"abc.png","yum","food6"));
+        
+        menues.add(new Menu(1,Approval.Pending,new Date(System.currentTimeMillis()),MealType.Breakfast,items.subList(0, 3)));
+        menues.add(new Menu(2,Approval.Pending,new Date(System.currentTimeMillis()),MealType.Lunch,items.subList(3, 6)));
+        menues.add(new Menu(3,Approval.Approved,new Date(System.currentTimeMillis()),MealType.Lunch,items));
+        
+        // Mockito.when(itemService.saveAll(items)).thenReturn( items);
+        Mockito.when(menuRepo.findAll()).thenReturn( menues);
+        for(Menu i:menues){
+            Mockito.when(menuRepo.save( Mockito.eq(i))).thenReturn( i);
+            Mockito.when(menuRepo.findById(  Mockito.eq(i.getId()))).thenReturn( Optional.of(i));
+            // Mockito.when(menuRepo.deleteById(  Mockito.eq(i.getId())));
+            
+        }
+        Mockito.when(menuRepo.findById( Mockito.eq(11))).thenThrow(NotFoundException.class);
+        Mockito.when(menuRepo.findById( Mockito.eq(12))).thenThrow(NotFoundException.class);
+     
+        // Mockito.when(itemService.saveAll(items)).thenReturn( items);
+        // Mockito.when(itemService.findAll()).thenReturn( items);
+        for(Item i:items){
+            // Mockito.when(itemService.save( Mockito.eq(i))).thenReturn( i);
+            Mockito.when(itemService.getItem(  Mockito.eq(i.getId()))).thenReturn( i );
+        }
+        // Mockito.when(itemService.findById( Mockito.eq(11))).thenThrow(NotFoundException.class);
+        // Mockito.when(itemService.findById( Mockito.eq(12))).thenThrow(NotFoundException.class);
+
+        // when(ItemService.getItems("abc2@gamil.com")).thenReturn( items.get(1));
+        // when(ItemService.getItems("abc3@gamil.com")).thenReturn( items.get(2));
+        // when(ItemService.getItems("abc4@gamil.com")).thenReturn( items.get(3));
+    }
+    
+    @AfterEach
+    void destroy(){
+        items.clear();
+    }
+
+    @Test
+    void testAddItemToMenu() {
+        assertEquals(menues.get(0), 
+            menuService.addItemToMenu(menues.get(0).getId(), items.get(4).getId()));
+        
+        assertEquals(null, 
+            menuService.addItemToMenu(menues.get(0).getId(), 8));
+
+        assertEquals(null, 
+            menuService.addItemToMenu(menues.get(0).getId(), items.get(1).getId()));
+    
+    }
+
+    @Test
+    void testAddItemToMenu2() {
+
+    }
+
+    @Test
+    void testAddItemsToMenu() {
+
+    }
+
+    @Test
+    void testAddMenu() {
+
+        assertEquals(menues.get(0) , menuService.addMenu(menues.get(0)));
+        assertEquals(menues.get(1) , menuService.addMenu(menues.get(1)));
+        assertEquals(menues.get(2) , menuService.addMenu(menues.get(2)));
+    }
+
+    @Test
+    void testApproveMenu() {
+        assertEquals(true, menuService.approveMenu(menues.get(0), 1));
+        assertEquals(false, menuService.approveMenu(menues.get(2), 2));
+    }
+
+    @Test
+    void testGetAllMenu() {
+        assertArrayEquals(menues.toArray() , menuService.getAllMenu().toArray());
+    }
+
+    @Test
+    void testGetMenu() {
+        assertEquals(menues.get(0) , menuService.getMenu(1));
+        assertEquals(menues.get(1) , menuService.getMenu(2));
+        assertEquals(menues.get(2) , menuService.getMenu(3));
+        NotFoundException e1 = assertThrows(NotFoundException.class, ()-> menuService.getMenu(11));
+        NotFoundException e2 = assertThrows(NotFoundException.class, ()-> menuService.getMenu(12));
+        // assertEquals(" not found." , e1.getMessage());
+        // assertEquals(" not found." , e2.getMessage());
+    }
+
+    @Test
+    void testGetMenuByDate() {
+
+    }
+
+    @Test
+    void testIsServingToday() {
+
+    }
+
+    @Test
+    void testIsServingToday2() {
+
+    }
+
+    @Test
+    void testRemoveItemFromMenu() {
+
+    }
+
+    @Test
+    void testRemoveMenu() {
+        assertEquals(menues.get(0), menuService.removeMenu(menues.get(0).getId()).get());
+        assertEquals(menues.get(1), menuService.removeMenu(menues.get(1).getId()).get());
+        NotFoundException e1 = assertThrows( NotFoundException.class , ()-> menuService.removeMenu(11));
+        
+        Mockito.verify(menuRepo,Mockito.times(2)).deleteById(Mockito.any());
+    }
+}
