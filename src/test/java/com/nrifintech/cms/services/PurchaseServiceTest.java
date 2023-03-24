b90684ca5960194bbd59f3c6843249ad5f92a774
Sagnik Sinha <sagniks@trainee.nrifintech.com>
2023-03-24 18:15:32 +0530

    Added few tests for purchase

--- src/test/java/com/nrifintech/cms/services/PurchaseServiceTest.java
+++ src/test/java/com/nrifintech/cms/services/PurchaseServiceTest.java
@@ -1,8 +1,15 @@
 package com.nrifintech.cms.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -13,6 +20,14 @@ import com.nrifintech.cms.entities.Purchase;
 import com.nrifintech.cms.repositories.InventoryRepo;
 import com.nrifintech.cms.repositories.PurchaseRepo;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+
+
 @ExtendWith(MockitoExtension.class)
 public class PurchaseServiceTest {
 
@@ -22,24 +37,41 @@ public class PurchaseServiceTest {
     @Mock
     private InventoryRepo inventoryRepo;
 
+    @InjectMocks
+    private PurchaseService purchaseService;
+
     Inventory inventorySample;
     Purchase purchaseSample;
+    List<Purchase> purchases;
 
     @Before
     public void setup(){
         MockitoAnnotations.openMocks(this);
         this.inventorySample = new Inventory(10, "potato", 10.09, 20.09, null);
         this.purchaseSample = new Purchase(100,2.0,12,null,null);
+        this.purchases = new ArrayList<Purchase>(){{
+            for( int i = 0 ;i < 1000 ; i++){
+                add( new Purchase(i,20.0+i,1100.0+i,null,null) );
+            }
+        }};
     }
 
     @Test
-    public void testGetAllPurchase() {
-
+    public void testGetAllPurchaseNonEmpty() {
+        Mockito.when( this.purchaseRepo.findAll() ).thenReturn(purchases);
+        assertArrayEquals(purchases.toArray(), this.purchaseService.getAllPurchase().toArray() );
+    }
+    @Test
+    public void testGetAllPurchaseEmpty() {
+        Mockito.when( this.purchaseRepo.findAll() ).thenReturn( new ArrayList<>() );
+        assertEquals(0 , this.purchaseService.getAllPurchase().size() );
     }
 
     @Test
     public void testGetPurchaseById() {
-
+        Mockito.when( this.purchaseRepo.findById(100) ).thenReturn(Optional.of(this.purchaseSample));
+        assertEquals(this.purchaseSample, this.purchaseService.getPurchaseById(100));
+        assertEquals(null, this.purchaseService.getPurchaseById(10));
     }
 
     @Test
